9c721aa2c5969b1315e1e20b893219a823a76931
hyh <hnu_huyunhao1995@163.com>
2026-08-03 19:06:05 +0800

    添加注释

--- src/planning/planner.h
+++ src/planning/planner.h
@@ -26,13 +26,13 @@ class Planner {
   LocalPlanner local_planner_;
   SpeedPlanner speed_planner_;
   HybridAStar hybrid_a_star_;
-  Mode mode_ = Mode::kLaneApproach;
-  std::string active_target_parking_spot_id_;
-  ParkingManeuver parking_maneuver_;
-  size_t parking_segment_index_ = 0;
-  uint64_t gear_shift_start_timestamp_ns_ = 0;
-  uint64_t last_parking_replan_timestamp_ns_ = 0;
-  DrivingDirection pending_direction_ = DrivingDirection::kUnknown;
+  Mode mode_ = Mode::kLaneApproach;              // 内部状态机
+  std::string active_target_parking_spot_id_;    // 当前激活的车位id，用于检测任务是否切换
+  ParkingManeuver parking_maneuver_;             // Hybrid A* 生成的完整泊车动作，包含一个或多个同向行驶段。
+  size_t parking_segment_index_ = 0;             // 当前正在执行的泊车段的下标
+  uint64_t gear_shift_start_timestamp_ns_ = 0;   // 换挡开始时刻时间戳
+  uint64_t last_parking_replan_timestamp_ns_ = 0;// 最近一次泊车重规划时间戳
+  DrivingDirection pending_direction_ = DrivingDirection::kUnknown;  // 行驶方向
 };
 }  // avp 命名空间
 #endif  // 头文件保护
